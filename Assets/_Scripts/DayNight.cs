bc5bd19969a7401e51e1030037ba43e3f87dab7c
mike-935 <Michael935935@gmail.com>
2023-04-11 22:16:22 -0400

    Changed the DayNight to work with Azure

--- Assets/_Scripts/DayNight.cs
+++ Assets/_Scripts/DayNight.cs
@@ -3,23 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AzureSky;
 
 public class DayNight : MonoBehaviour
 {
-    public Light sunLight;
+    // public Light sunLight;
     public float duration = 20f;
     public float timeInterval = 1f;
-    public float amount = 1.0f;
+    // public float amount = 1.0f;
+    public float startValue = 0;
+    public float endValue = 2.03f;
+    public AzureTimeController atc;
     private float _cycles = 0f;
     private bool _firstEnter = true;
     private bool _start = false;
     private float _elapsedTime = 0.0f;
-    
+    private float _tVal = 0f;
+    private float _timeValue;
     // Update is called once per frame
     void Update()
     {
         if (_start)
         {
+            _elapsedTime += Time.deltaTime;
+            if (_elapsedTime >= timeInterval && _cycles <= duration)
+            {
+                _elapsedTime %= timeInterval;
+                _tVal += Time.time;
+                _timeValue = Mathf.Lerp(startValue, endValue, _tVal / duration);
+                atc.SetTimeline(_timeValue);
+                _cycles++;
+            }
+            /*
             _elapsedTime += Time.deltaTime;
             if (_elapsedTime >= timeInterval && _cycles <= duration)
             {
@@ -30,6 +45,7 @@ public class DayNight : MonoBehaviour
             {
                 _start = false;
             }
+            */
         }
     }
 

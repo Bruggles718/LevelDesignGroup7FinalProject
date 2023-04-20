17a0f17b3d5cb80def3cfaede1cdab7d9c7361d2
Bradley Karmin <bkarmin718@gmail.com>
2023-04-20 16:47:17 -0400

    added troll roar sound effect

--- Assets/Troll.cs
+++ Assets/Troll.cs
@@ -7,6 +7,9 @@ public class Troll : MonoBehaviour
 {
 
     private Animator _animator;
+    public AudioClip clip;
+    public int volume = 1;
+    public float roarSoundDelay = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +20,15 @@ public class Troll : MonoBehaviour
     public void Roar()
     {
         _animator.Play("Troll_Roar");
+        StartCoroutine(RoarSoundCo());
+    }
+
+    IEnumerator RoarSoundCo()
+    {
+        yield return new WaitForSeconds(roarSoundDelay);
+        for (int i = 0; i < volume; i += 1)
+        {
+            AudioSource.PlayClipAtPoint(clip, this.transform.position);
+        }
     }
 }
